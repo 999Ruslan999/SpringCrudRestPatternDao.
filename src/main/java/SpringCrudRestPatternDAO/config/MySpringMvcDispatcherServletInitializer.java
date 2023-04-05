8cb0c73fffbbc37da010db5f6d4b3eff27b06724
Ruslan Popov <ruslan_popov_2014@inbox.ru>
2023-04-05 12:25:29 +0300

    initial commit

--- src/main/java/SpringCrudRestPatternDAO/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/SpringCrudRestPatternDAO/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,2 +1,38 @@
-package SpringCrudRestPatternDAO.config;public class MySpringMvcDispatcherSerlvetIntitializer {
+package SpringCrudRestPatternDAO.config;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author Neil Alishev
+ */
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
 }
